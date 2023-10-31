d4e42c2a2c92724d11ffa4d82b992276db858f66
gabriel-tr <slashnebulous02@gmail.com>
2023-10-31 18:29:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const content = document.querySelector(".result");
+const message = document.querySelector("#message");
+
+var vendaArea = document.querySelector("#venda p");
+var porcentagemArea = document.querySelector("#porcentagem p");
+var gastoArea = document.querySelector("#gasto p");
+var lucroNumber = document.querySelector("#lucroNumber p");
+
+const button = document
+  .querySelector("#mainButton")
+  .addEventListener("click", () => {
+    let valorDoCelular = Number(document.querySelector("#celular").value);
+    let valorDaTela = Number(document.querySelector("#tela").value);
+    let porcentagemFinal = Number(document.querySelector("#percentage").value);
+
+    content.style.display = "flex";
+    message.style.display = "flex";
+
+    let soma = valorDoCelular + valorDaTela;
+    let novoValor = soma * (1 + porcentagemFinal / 100);
+    let math = novoValor - soma;
+    let final = soma + math;
+
+    gastoArea.textContent = soma + "R$";
+    porcentagemArea.textContent = porcentagemFinal + "%";
+    vendaArea.textContent = novoValor + "R$";
+    lucroNumber.textContent = math + "R$";
+
+    document.querySelector("#present").textContent = soma + "R$";
+    document.querySelector("#finalResult").textContent = final + "R$";
+    console.log(novoValor);
+  });
